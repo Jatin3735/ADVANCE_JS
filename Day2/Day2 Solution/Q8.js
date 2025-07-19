f24babfb83935c5f4905e34c385a5056161a0749
unknown <jatinjangra514@gmail.com>
2025-07-19 12:37:05 +0530

    hello

--- Day2/Day2 Solution/Q8.js
+++ Day2/Day2 Solution/Q8.js
@@ -1,13 +1,35 @@
-let n = 9;
-let count= 0
-for(let i=2;i<=n/2;i++){
-    if(n%i==0){
-        count++
+// let n = 9;
+// let count= 0
+// for(let i=2;i<=n/2;i++){
+//     if(n%i==0){
+//         count++
+//     }
+// }
+// if(count!=0 && n>1){
+//     console.log("not prime");
+// }
+// else{
+//     console.log("prime");
+// }
+
+
+let n = 15;
+if(n>1){
+    let fact = 0;
+    for(let i=1;i<=n;i++){
+        if(n%i==0){
+            fact++
+            console.log(i);
+        }
+    }
+    if(fact==2){
+        console.log("Prime");
+    }
+    else{
+        console.log("Not Prime");
     }
 }
-if(count!=0 && n>1){
+else{
     console.log("not prime");
 }
-else{
-    console.log("prime");
-}
+
